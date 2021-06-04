ad77280f6b32717fd2fae327ae368b6d19d5aae4
Dylan Trecker <dylantrecker@WarMachineAir.local>
2021-06-04 01:47:34 -0700

    made routes for index and notes html

--- server.js
+++ server.js
@@ -9,10 +9,19 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.static('public'));
 
-app.get('/api/db', (req, res) => {
-    res.json(notes);
-})
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public.html'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+
+// app.get('/api/db', (req, res) => {
+//     res.json(notes);
+// });
 
 app.listen(3001, () => {
     console.log('API server now on port 3001');
-})
+});

0a0e065b02327fa28fe9004273af8524f8aa4b99
Dylan Trecker <dylantrecker@WarMachineAir.local>
2021-06-04 03:16:37 -0700

    Added correct port

--- server.js
+++ server.js
@@ -5,18 +5,20 @@ const path = require('path');
 const PORT = process.env.PORT || 3001;
 const savedNotes = require("./db/notes.json");
 
+//middleware
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.static('public'));
 
+//GET request to render public HTML
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public.html'));
 });
-
+//GET request to render notes HTML
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
 });
-
+//GET request for notes in DB. 
 app.get('/api/notes', (req, res) => {
     res.sendFile(path.join(__dirname, "/db/notes.json"));
 });
@@ -25,6 +27,7 @@ app.get('/api/notes', (req, res) => {
 //     res.json(savedNotes);
 // });
 
-app.listen(3001, () => {
-    console.log('API server now on port 3001');
+//Listen request
+app.listen(PORT, () => {
+    console.log(`API server is now on ${PORT}!`);
 });
